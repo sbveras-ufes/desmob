fc86b167302a26f0bb71728e6acbde782025e46e
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-09-17 09:50:11 -0300

    Updated User.ts

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,7 +2,7 @@ export interface User {
   id: string;
   nome: string;
   email: string;
-  cargo: 'Gestor Contrato' | 'Supervisor';
+  cargo: 'Gestor Contrato' | 'Supervisor' | 'Diretor';
   cr: string[];
   createdAt: string;
   updatedAt: string;
@@ -11,13 +11,13 @@ export interface User {
 export interface UserFilters {
   nome?: string;
   email?: string;
-  cargo?: 'Gestor Contrato' | 'Supervisor' | '';
+  cargo?: 'Gestor Contrato' | 'Supervisor' | 'Diretor' | '';
   cr?: string[];
 }
 
 export interface UserFormData {
   nome: string;
   email: string;
-  cargo: 'Gestor Contrato' | 'Supervisor' | '';
+  cargo: 'Gestor Contrato' | 'Supervisor' | 'Diretor' | '';
   cr: string[];
 }
