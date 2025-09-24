69b4f8b4a27a7a0a44234bb49cb61fddf0830cb6
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-09-24 12:15:08 -0300

    Updated mockFlows.ts

---
+++ src/data/mockFlows.ts
@@ -0,0 +1,24 @@
+import { ApprovalFlow } from '../types/Flow';
+
+export const mockFlows: ApprovalFlow[] = [
+  {
+    id: 'flow-1',
+    description: 'Fluxo Padrão Operações LETS',
+    diretoria: 'LETS',
+    crs: ['301045540', '301045547'],
+    users: [
+      { id: 1, userId: '2', role: 'Análise Supervisor' },
+      { id: 2, userId: '1', role: 'Análise Gerente' },
+    ]
+  },
+  {
+    id: 'flow-2',
+    description: 'Fluxo Diretoria Comercial',
+    diretoria: 'COMERCIAL',
+    crs: ['301045541'],
+    users: [
+      { id: 1, userId: '2', role: 'Análise Supervisor' },
+      { id: 2, userId: '4', role: 'Análise Diretor' },
+    ]
+  }
+];
