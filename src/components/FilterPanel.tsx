be6950fb07ec703fe8ff9c0c9a6eef1bd3e7c627
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-09-24 17:41:05 -0300

    Updated FilterPanel.tsx

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -267,7 +267,7 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ filters, onFiltersChange, act
                 >
                   <option value="">Todas as Situações</option>
                   <option value="Aguardando aprovação">Aguardando aprovação</option>
-                  <option value="Aprovado">Aprovado</option>
+                  <option value="Liberado para Desmobilização">Liberado para Desmobilização</option>
                   <option value="Reprovado">Reprovado</option>
                 </select>
               </div>
