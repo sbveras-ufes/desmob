654da35b2d1be7aba9ab31f33d97b63a4ae69035
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-09-29 21:27:37 -0300

    Updated AcompanhamentoTable.tsx

--- src/components/AcompanhamentoTable.tsx
+++ src/components/AcompanhamentoTable.tsx
@@ -109,7 +109,7 @@ const AcompanhamentoTable: React.FC<AcompanhamentoTableProps> = ({ vehicles, sel
                 Situação
               </th>
               <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Situação Análise Fiscal
+                Situação Análise Documental
               </th>
               <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Situação da Vistoria
@@ -162,8 +162,8 @@ const AcompanhamentoTable: React.FC<AcompanhamentoTableProps> = ({ vehicles, sel
                   </span>
                 </td>
                 <td className="px-2 py-2 text-sm">
-                   <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${getSituacaoColor(vehicle.situacaoAnaliseFiscal)}`}>
-                    {vehicle.situacaoAnaliseFiscal || '-'}
+                   <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${getSituacaoColor(vehicle.situacaoAnaliseDocumental)}`}>
+                    {vehicle.situacaoAnaliseDocumental || '-'}
                   </span>
                 </td>
                 <td className="px-2 py-2 text-sm text-gray-500">{vehicle.situacaoVistoria || '-'}</td>
