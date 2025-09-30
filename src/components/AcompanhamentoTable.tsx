c6fe28334ffc1879fe8c275993ef241ad671bb93
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-09-29 21:15:06 -0300

    Updated AcompanhamentoTable.tsx

--- src/components/AcompanhamentoTable.tsx
+++ src/components/AcompanhamentoTable.tsx
@@ -32,10 +32,10 @@ const AcompanhamentoTable: React.FC<AcompanhamentoTableProps> = ({ vehicles, sel
         return 'bg-red-100 text-red-800';
       case 'Liberado para Transferência':
         return 'bg-blue-100 text-blue-800';
-      case 'Aprovada':
+      case 'Documentação Aprovada':
         return 'bg-green-100 text-green-800';
-      case 'Pendente':
-        return 'bg-yellow-100 text-yellow-800';
+      case 'Documentação Pendente':
+        return 'bg-red-100 text-red-800';
       default:
         return 'bg-gray-100 text-gray-800';
     }
