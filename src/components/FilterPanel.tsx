54f62901aba1a4d70711bb29d1d3e2632fd4e3bd
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-10-08 15:42:09 -0300

    Updated FilterPanel.tsx

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -268,7 +268,7 @@ const FilterPanel: React.FC<FilterPanelProps> = ({ filters, onFiltersChange, act
             </select>
             <select value={filters.patioDestino || ''} onChange={(e) => handleFilterChange('patioDestino', e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <option value="">Selecione o Pátio</option>
+                <option value="">Selecione o Pátio de Destino</option>
                 {uniqueValues.patiosDestino.map(patio => <option key={patio} value={patio}>{patio}</option>)}
             </select>
 
