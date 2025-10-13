c4fb5fc2e94a7ca7012e50f28ead02fd65f62080
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-10-13 20:22:51 -0300

    Updated AcompanhamentoDesmobilizacaoTab.tsx

--- src/components/AcompanhamentoDesmobilizacaoTab.tsx
+++ src/components/AcompanhamentoDesmobilizacaoTab.tsx
@@ -22,7 +22,7 @@ const AcompanhamentoDesmobilizacaoTab: React.FC<AcompanhamentoDesmobilizacaoTabP
         </p>
         {vehicles.length > 0 && (
           <div className="flex items-center space-x-2">
->
+            
             <button className="flex items-center space-x-2 px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 text-sm font-medium">
               <Download size={16} />
               <span>Exportar</span>
