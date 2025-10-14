cd41dddaeb812401778507c25724453d1dcb4f17
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-10-14 19:03:30 -0300

    Updated mockCompanies.ts

---
+++ src/data/mockCompanies.ts
@@ -0,0 +1,5 @@
+export const mockCompanies = [
+  { nome: 'Empresa Alpha', cnpj: '11.111.111/0001-11' },
+  { nome: 'Transportadora Beta', cnpj: '22.222.222/0001-22' },
+  { nome: 'Logística Gamma', cnpj: '33.333.333/0001-33' },
+];
