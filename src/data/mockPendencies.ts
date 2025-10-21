868a56fa2384e48134cb1fa8818d75e5fb85dc12
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-10-21 09:47:39 -0300

    Updated mockPendencies.ts

--- src/data/mockPendencies.ts
+++ src/data/mockPendencies.ts
@@ -6,4 +6,8 @@ export const mockPendencies: Pendency[] = [
   { id: '3', origem: 'Documental', descricao: 'Recall', geraBloqueio: false },
   { id: '4', origem: 'Fiscal', descricao: 'IPVA Atrasado', geraBloqueio: true },
   { id: '5', origem: 'Fiscal', descricao: 'Licenciamento Vencido', geraBloqueio: true },
+  { id: '6', origem: 'Manutenção', descricao: 'Motor', geraBloqueio: false },
+  { id: '7', origem: 'Manutenção', descricao: 'Elétrica', geraBloqueio: false },
+  { id: '8', origem: 'Manutenção', descricao: 'Freios', geraBloqueio: false },
+  { id: '9', origem: 'Manutenção', descricao: 'Suspensão', geraBloqueio: false },
 ];
