8d06616b416d8e5d72c9a688e97ca92ccf55fbe4
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-11-03 17:54:02 -0300

    Updated FlowManagementPage.tsx

--- src/pages/FlowManagementPage.tsx
+++ src/pages/FlowManagementPage.tsx
@@ -43,7 +43,7 @@ const FlowManagementPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="px-4 sm:px-6 lg:px-8 py-8">
         <UserBreadcrumb />
         {view === 'list' ? (
           <FlowListPage flows={flows} onCreateNew={handleCreateNew} onEdit={handleEdit} onDelete={handleDelete} />
