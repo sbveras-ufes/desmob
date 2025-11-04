a8f790090368c7302c3e9a5a51ae521645c1e385
sbveras-ufes <108524052+sbveras-ufes@users.noreply.github.com>
2025-11-04 14:16:43 -0300

    Updated ApprovalTable.tsx

--- src/components/ApprovalTable.tsx
+++ src/components/ApprovalTable.tsx
@@ -41,6 +41,8 @@ const ApprovalTable: React.FC<ApprovalTableProps> = ({
         return 'bg-green-100 text-green-800';
       case 'Reprovado':
         return 'bg-red-100 text-red-800 hover:bg-red-200 cursor-pointer';
+      case 'Em Andamento': // Novo status
+        return 'bg-cyan-100 text-cyan-800';
       default:
         return 'bg-gray-100 text-gray-800';
     }
